29118077c270290964e93487eb17548351d458fb
Dmytro Kozynets <Admin@MacBook-Pro-Dmytro.local>
2017-09-11 00:44:42 +0300

    validate from model db

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,36 +1,47 @@
 const db = require('./../models');
 const config = require('./../config/env.js');
 const validator = require('validator');
-var jwt = require('jwt-simple');
+const jwt = require('jwt-simple');
+const sequelize = require('sequelize');
 function validateUser(user){
-    if ( user && user.username && user.password ){
-        return !validator.isEmpty(user.username) && !validator.isEmpty(user.password);
+    if ( user && user.email && user.password ){
+        return !validator.isEmpty(user.email) && !validator.isEmpty(user.password);
     }else{
         return false;
     } 
 }
 function signup(req, res, next){
     if (!validateUser(req.body)){
-        res.status(401);
-        res.json({success: false, message: 'username, password is required'});
+        res.status(422);
+        res.json({success: false, message: 'email , password is required'});
     }; 
-    const {username, password} = req.body;
-    db.User.create({username, password}).then((newUser)=>{
-        res.json({
-            success: true,
-            message: 'User has created'
-        });
-    }).catch((err)=>{
-        next(err);
-    })
+    const {email, password} = req.body;
+    const getUserByEmail = db.User.findOne({where: {email}});
+    const createUser = function({email, password}){
+        db.User.create({email, password}).then((newUser)=>{
+            res.json({
+                success: true,
+                message: 'User has created'
+            });
+        })
+        .catch(sequelize.ValidationError, function (err) {
+            // respond with validation errors
+            res.status(422);
+            res.json({success: false, message: err.message});
+        })
+        .catch((err)=>{
+            next(err);
+        })
+    }
+    createUser({email, password});
 }
 function signin(req, res, next){
     if (!validateUser(req.body)){
         res.status(401);
-        res.json({success: false, message: 'username & password is required'});
+        res.json({success: false, message: 'email & password is required'});
     };
-    const { username, password} = req.body;
-    db.User.findOne({where:{username}}).then( (user)=>{
+    const { email, password} = req.body;
+    db.User.findOne({where:{email}}).then( (user)=>{
         if (!user){
             res.status('401');
             res.json({success: false, message: 'User not found'});
--- server/models/user.js
+++ server/models/user.js
@@ -4,9 +4,34 @@ const bcrypt = require('bcrypt');
 module.exports = function (sequelize, DataTypes) {
   const User = sequelize.define('User', {
     username: DataTypes.STRING,
-    password: { 
+    email: {
       type: DataTypes.STRING,
-      validate: { 
+      unique: true,
+      validate: {
+        isEmail: true,
+        isUnique: function (value, next) {
+          const self = this;
+          User.find({
+              where: {
+                email: value
+              }
+            })
+            .then(function (user) {
+              // reject if a different user wants to use the same email
+              if (user && self.id !== user.id) {
+                return next('Email already in use!');
+              }
+              return next();
+            })
+            .catch(function (err) {
+              return next(err);
+            });
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      validate: {
         notEmpty: true
       }
     }
@@ -17,14 +42,14 @@ module.exports = function (sequelize, DataTypes) {
       }
     }
   });
-  User.beforeSave((user, options) => { 
-    if ( user.changed('password') ){
+  User.beforeSave((user, options) => {
+    if (user.changed('password')) {
       let hash = bcrypt.hashSync(user.password, 10);
       user.password = hash;
     }
   });
-  User.prototype.comparePassword = function(password){
-    return bcrypt.compareSync(password, this.password); 
+  User.prototype.comparePassword = function (password) {
+    return bcrypt.compareSync(password, this.password);
   }
   return User;
 };
